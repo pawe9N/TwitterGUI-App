d2275862142464e6e590406b579338ec702a86a5
pawe9N <zerratul@wp.pl>
2018-02-04 23:51:37 +0100

    Added twitter_connector_class

---
+++ sample/keys_class.py
@@ -0,0 +1,6 @@
+class Keys:
+	def __init__(self):
+		self.consumer_key = "vov1TLE0E4BH67Liwe2Ucv9wa"
+		self.consumer_secret = "fcY4r328UqbGZFsi9uAZHKdRwWv4RBGdm2SEFSs6QHOuRk4jPd"
+		self.access_token = "959911508573589505-RJWDy1tZDE0HBygvWrXcHuxfqmjYZH7"
+		self.access_secret = "THZVU2h64zdQ1rzi3cbhH3iH633w6GP7s0l4w903YPmEH"
---
+++ sample/twitter_connector_class.py
@@ -0,0 +1,34 @@
+import twitter
+from keys_class import Keys
+
+class Twitter_Connector():
+	def __init__(self):
+		self.keys = Keys()
+		self.api = twitter.Api(consumer_key=self.keys.consumer_key,
+				consumer_secret=self.keys.consumer_secret,
+				access_token_key=self.keys.access_token,
+				access_token_secret=self.keys.access_secret)
+	def get_credentials(self):
+		return self.api.VerifyCredentials()
+
+	def get_user_name(self):
+		return self.api.VerifyCredentials().name
+
+	def get_user_image_url(self):
+		return self.api.VerifyCredentials().profile_image_url
+
+	def get_user_screen_name(self):
+		return self.api.VerifyCredentials().screen_name
+
+	def get_friends(self):
+		return self.api.GetFriends()
+
+	def post_status_message(self, status_message):
+		return self.api.PostUpdate(status_message)
+
+	
+
+Tconnector = Twitter_Connector()
+
+print(Tconnector.get_credentials())
+print(Tconnector.get_friends())
